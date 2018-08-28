04917e43094f5ac33e264ab60a6d7f0458fede73
eeghor <eeghor@gmail.com>
2018-08-28 23:36:28 +1000

    need to grab reviewers info

--- scrape_code.py
+++ scrape_code.py
@@ -4,77 +4,100 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from collections import defaultdict
+import re
+import json
+import os
+import arrow
 
 
 class SnoozeReviews:
 
 	def __init__(self, base_url='https://www.productreview.com.au/p/snooze.html'):
 
+		self.today = arrow.utcnow().to('Australia/Sydney').format('YYYYMMDD')
 		self.base_url = base_url
 		self.total_reviews = None
 		self.review_counts = defaultdict()
+		self.reviews = []
+
+		if not os.path.exists('data'):
+			os.mkdir('data')
+
+		self.data_dir = 'data'
 
 	def page_one(self):
 
-		sp = BeautifulSoup(requests.get(self.base_url).text, 'html.parser')
+		pages_ = int(re.search(r'\d+\)', BeautifulSoup(requests.get('https://www.productreview.com.au/p/snooze/2.html').text, 'html.parser').find('div', class_='item-header-summary-title').text.lower().strip()).group(0).split(')')[0])
+		
+		for p in range(1, pages_ + 1):
 
-		for _ in sp.find_all('span', itemprop='ratingCount'):
+			sp = BeautifulSoup(requests.get(f'https://www.productreview.com.au/p/snooze/{p}.html').text, 'html.parser')
 
-			try:
-				self.total_reviews = int(_.text.strip())
-			except:
-				continue
+			if p == 1:
 
-		print(f'found {self.total_reviews} snooze reviews...')
+				for _ in sp.find_all('span', itemprop='ratingCount'):
+	
+					try:
+						self.total_reviews = int(_.text.strip())
+					except:
+						continue
+	
+				print(f'found {self.total_reviews} snooze reviews...')
 
-		for _ in sp.find_all('li', class_='rating-overview-row'):
+				for _ in sp.find_all('li', class_='rating-overview-row'):
 			
-			for r_, d_ in zip(_.find_all('div', class_='rate'), _.find_all('div', class_='count')):
+					for r_, d_ in zip(_.find_all('div', class_='rate'), _.find_all('div', class_='count')):
 
-				self.review_counts[r_.text.lower()] = d_.text.lower()
-		print(self.review_counts)
+						self.review_counts[r_.text.lower()] = int(d_.text.lower())
+
+				self.reviews.append(self.review_counts)
+
+			for review in sp.find_all('div', class_='review'):
+
+				this_review = defaultdict()
+
+				auth = review.find('a', itemprop='author')
+
+				if not auth:
+					continue
+
+				lnk = auth['href']
+
+				this_review['author'] = auth.text.lower().strip()
+				this_review['author_link'] = 'https://www.productreview.com.au' + lnk
+
+				shop = defaultdict()
+
+				try:
+					shop['state'], shop['suburb'] = [_.split(':')[-1].strip() for _ in review.find('p', class_='review-labels').text.strip().lower().split(',')]
+				except:
+					pass
+
+				this_review['shop'] = shop
+
+				this_review['title'] = review.find('h3', itemprop='name').text.lower().strip()
+
+				try:
+					rating_ = int(review.find('span', itemprop='ratingValue').text.strip())
+				except:
+					rating_ = None
+
+				this_review['rating'] = rating_
+
+				this_review['date'] = review.find('meta', itemprop='datePublished')['content']
+
+				this_review['text'] = review.find('div', itemprop='description').text.lower().strip()
+
+				self.reviews.append(this_review)
+
+			print(f'pages: {p:02d}, reviews: {len(self.reviews):03d}...')
+
+
+		json.dump(self.reviews, open(os.path.join(self.data_dir, f'reviews_{self.today}.json'), 'w'))
 
 		return self
 
 if __name__ == '__main__':
 
-	sr = SnoozeReviews().page_one()
-
-
-
-
-
-
-
-#specify list of the webpages in interest
-# pages = ['https://...']
-
-# review_heading = []
-# review_content = []
-# review_rating = []
-# review_date = []
-
-# for page in pages:
-#     html = requests.get(page)
-#     soup = BeautifulSoup(html.text, 'html.parser')
-# #     ws_title = soup.title.string
-# #     print soup.prettify()
-#     all_reviews = soup.find_all('div', class_ = 'review-content')
-
-#     for review in all_reviews:
-#         review_heading.append(review.find('h3').text.strip())
-#         review_content.append(review.find('div', attrs = {'class':'review-overall'}).text.strip())
-#         review_rating.append(review.find('span', attrs = {'itemprop':'ratingValue'}).text.strip())
-#         review_date.append(str(review.find('meta', attrs = {'itemprop':'datePublished'})).split('"')[1])
-
-
-# data = {'Review Title' : review_heading, 'Review Content': review_content,
-#         'Review Rating': review_rating, 'Review Date': review_date}
-
-# #create review dataframe
-# review_df = pd.DataFrame(data, columns = ['Review Date','Review Title', 'Review Content', 'Review Rating'])
-# review_df.sort_values('Review Date', ascending = False, inplace = True)
-
-
-# #export to a path as csv file
-# review_df.to_csv('path')
+	sr = SnoozeReviews() \
+		.page_one()
